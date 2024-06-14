1fcce9bf477b6393ec8a641cfe9c56a029f3e52a
Hezekiah Elisha <elishahezekiah903@gmail.com>
2024-06-14 19:59:36 +0300

    chore: Add chart.js and react-chartjs-2 dependencies
    This commit adds the chart.js and react-chartjs-2 dependencies to the project. These libraries will be used to implement charts and data visualization in the application.

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,16 +1,15 @@
 import { Outlet, useNavigate } from "react-router-dom"
 import DashboardHeader from "../components/DashboardHeader";
 import SideNav from "../components/SideNav";
-import { useEffect } from "react";
 
 
 export default function DashboardPage() {
     const navigation = useNavigate();
 
-    useEffect(() => {
-        // Redirects to /dashboard/home immediately when this component mounts
-        navigation('/dashboard/home');
-    }, [navigation]); // Dependency array with navigate to ensure effect is run once
+    // useEffect(() => {
+    //     // Redirects to /dashboard/home immediately when this component mounts
+    //     navigation('/dashboard/home');
+    // }, [navigation]); // Dependency array with navigate to ensure effect is run once
 
     return (
         <div className="flex flex-col h-screen">
